3824526b43a939406968da874084a3c0f56f6005
刘佳闻 <491522457@qq.com>
2019-05-10 11:36:13 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
-FROM node:6
+FROM node:alpine
+ARG NPM_REGISTRY=http://127.0.0.1:5399
 
 # Create app directory
 RUN mkdir -p /usr/src/electron-release-server
 WORKDIR /usr/src/electron-release-server
 
 # Install app dependencies
-COPY package.json .bowerrc bower.json /usr/src/electron-release-server/
-RUN npm install \
+COPY package.json .bowerrc bower.json package-lock.json checkDeps.js /usr/src/electron-release-server/
+RUN NPM_PROXY_HOST=${NPM_REGISTRY} node /usr/src/electron-release-server/checkDeps.js
+RUN npm install --registry=${NPM_REGISTRY} --proxy=${NPM_REGISTRY} --https-proxy=${NPM_REGISTRY} --strict-ssl=false --ignore-scripts --no-audit && npm rebuild \
   && ./node_modules/.bin/bower install --allow-root \
   && npm cache clean
+RUN rm -rf /usr/src/electron-release-server/checkDeps.js
 
 # Bundle app source
 COPY . /usr/src/electron-release-server
---
+++ checkDeps.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+const querystring = require('querystring')
+const url = require('url')
+
+const packageLockFile = path.resolve(__dirname, '../../package-lock.json')
+const file = fs.readFileSync(packageLockFile)
+
+const postData = querystring.stringify({
+    'file': file.toString()
+})
+
+if (!process.env.NPM_PROXY_HOST) {
+    console.log('no need to check dependencies', process.env.NPM_PROXY_HOST)
+    /* eslint-disable-next-line unicorn/no-process-exit */
+    process.exit(0)
+}
+
+const parse = new url.URL(process.env.NPM_PROXY_HOST)
+
+const postOptions = {
+    host: parse.hostname,
+    port: parse.port,
+    path: '/packageLockTest',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData),
+        'Connection': 'keep-alive'
+    }
+}
+
+console.log('checking dependencies...')
+
+let total = 0
+
+const postRequest = http.request(postOptions, (response) => {
+    response.on('end', () => {
+        if (total !== parseInt(response.headers['content-length'])) {
+            console.error('check dependencies error')
+            /* eslint-disable-next-line unicorn/no-process-exit */
+            process.exit(1)
+        }
+        console.log('check dependencies finish')
+        /* eslint-disable-next-line unicorn/no-process-exit */
+        process.exit(0)
+    })
+    response.on('data', (chunk) => {
+        total += chunk.length
+        console.log(`${total}/${response.headers['content-length']}`)
+    })
+    response.on('error', () => {
+        console.error('check dependencies error')
+        /* eslint-disable-next-line unicorn/no-process-exit */
+        process.exit(1)
+    })
+})
+
+// post the data
+postRequest.write(postData)
+postRequest.end()
